d705b295ac6f972cae7bd8924505978dc212fca7
Christophe Irakoze <irachriskhan@gmail.com>
2023-09-02 20:01:17 +0200

    add services section

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,5 +51,37 @@ skillsHeader.forEach((el) =>{
 })
 
 /* ============== QUALIFICATION TABS ==================== */
+const tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.CDATA_SECTION_NODE.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
+
+
+/* ============== SERVIES MODEL ==================== */
+
+
+/* ============== PORTIFOLIO SWIPER ==================== */
+
+
+/* ============== TESTIMONIAL ==================== */
+
+
+/* ============== SCROLL SECTIONS ACTIVE LINK ==================== */
 
 
